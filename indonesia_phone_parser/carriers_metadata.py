5881bd15a336a83c195209a144130e3e2c4724fc
Steven <steven@ui.co.id>
2014-07-04 15:15:20 +0700

    Add area and carrier metadata

---
+++ indonesia_phone_parser/carriers_metadata.py
@@ -0,0 +1,35 @@
+CDMA_PREFIX = {
+
+    "9": "Esia",  # (Area-Code) - 9xxx-xxxx
+    "83": "Esia",  # 021-83xx-xxxx
+
+    "54": "TelkomFlexi",  # (area-code)-54xx-xxxx
+    "70": "TelkomFlexi",  # (area-code)-70xx-xxxx
+    "80": "TelkomFlexi",  # (area-code)-80xx-xxxx
+    "81": "TelkomFlexi",  # (area-code)-81xx-xxxx
+    "68": "TelkomFlexi",  # (area-code)-68xx-xxxx
+    "3": "TelkomFlexi",  # (area-code)-3xxx-xxxx
+
+    "61": "Indosat",  # (Area-code)-61xx-xxxx
+    "62": "Indosat",  # (Area-code)-62xx-xxxx
+    "63": "Indosat",  # (Area-code)-63xx-xxxx
+    "90": "Indosat",  # (Area-code)-90xx-xxxx
+    "30": "Indosat",  # 021-30xx-xxxx
+    "60": "Indosat",  # 021-60xx-xxxx
+
+    "50": "Hepi (Mobile-8)",  # (Area-code)-50xx-xxxx
+    "21": "Hepi (Mobile-8)",  # (Area-code)-21xx-xxxx
+    "31": "Hepi (Mobile-8)",  # (Area-code)-31xx-xxxx
+
+}
+
+CDMA_PREFIX_SPECIAL_AREA_CODE = {
+    "2180": "Esia",  # 021 - 80xx-xxxx
+    "2183": "Esia",  # 021 - 83xx-xxxx,
+
+    "2170": "TelkomFlexi",  # 021-70xx-xxxx
+    "2168": "TelkomFlexi",  # 021-68xx-xxxx
+
+    "2130": "Indosat",  # 021-30xx-xxxx
+    "3160": "Indosat",  # 031-60xx-xxxx
+}
--- indonesia_phone_parser/parser.py
+++ indonesia_phone_parser/parser.py
@@ -1,124 +1,74 @@
 import phonenumbers
-from phonenumbers import carrier
+# preload carrier
+from phonenumbers import carrier as phonenumber_carrier  # noqa
 
-
-AREA_CODE = {
-
-    "21": "Jakarta, Tangerang",
-    "252": "Lebak",
-    "253": "Pandeglang",
-    "254": "Cilegon, Serang",
-
-    "22": True,
-    "231": True,
-    "232": True,
-    "233": True,
-    "234": True,
-    "251": True,
-    "260": True,
-    "261": True,
-    "262": True,
-    "263": True,
-    "264": True,
-    "265": True,
-    "266": True,
-    "267": True,
-
-    '24': True,
-    '271': True,
-    '272': True,
-    '273': True,
-    '274': True,
-    '275': True,
-    '276': True,
-    '280': True,
-    '281': True,
-    '282': True,
-    '283': True,
-    '284': True,
-    '285': True,
-    '286': True,
-    '287': True,
-    '288': True,
-    '289': True,
-
-    '291': True,
-    '292': True,
-    '293': True,
-    '294': True,
-    '295': True,
-    '296': True,
-    '297': True,
-    '298': True,
-}
-
-CDMA_PREFIX_WITH_AREA_CODE = {
-
-    "9": "Esia",  # (Area-Code) - 9xxx-xxxx
-    "83": "Esia",  # 021-83xx-xxxx
-
-    "54": "TelkomFlexi",  # (area-code)-54xx-xxxx
-    "70": "TelkomFlexi",  # (area-code)-70xx-xxxx
-    "80": "TelkomFlexi",  # (area-code)-80xx-xxxx
-    "81": "TelkomFlexi",  # (area-code)-81xx-xxxx
-    "68": "TelkomFlexi",  # (area-code)-68xx-xxxx
-    "3": "TelkomFlexi",  # (area-code)-3xxx-xxxx
-
-    "61": "Indosat",  # (Area-code)-61xx-xxxx
-    "62": "Indosat",  # (Area-code)-62xx-xxxx
-    "63": "Indosat",  # (Area-code)-63xx-xxxx
-    "90": "Indosat",  # (Area-code)-90xx-xxxx
-    "30": "Indosat",  # 021-30xx-xxxx
-    "60": "Indosat",  # 021-60xx-xxxx
-
-    "50": "Hepi (Mobile-8)",  # (Area-code)-50xx-xxxx
-    "21": "Hepi (Mobile-8)",  # (Area-code)-21xx-xxxx
-    "31": "Hepi (Mobile-8)",  # (Area-code)-31xx-xxxx
-
-}
-
-CDMA_PREFIX_SPECIAL_AREA_CODE = {
-    "2180": "Esia",  # 021 - 80xx-xxxx
-    "2183": "Esia",  # 021 - 83xx-xxxx,
-
-    "2170": "TelkomFlexi",  # 021-70xx-xxxx
-    "2168": "TelkomFlexi",  # 021-68xx-xxxx
-
-    "2130": "Indosat",  # 021-30xx-xxxx
-    "3160": "Indosat",  # 031-60xx-xxxx
-}
+from indonesia_phone_parser.area_code_metadata import AREA_CODE
+from indonesia_phone_parser.carriers_metadata import CDMA_PREFIX_SPECIAL_AREA_CODE, CDMA_PREFIX
 
 
 class Parser(object):
 
-    phone = ''
-
-    province_name = ''
-    area_code = ''
-    area_name = ''
-
-    mobile_provider = ''
-
-    is_mobile = False
-
     def __init__(self, phone):
         self.phone = phone
+        self.province_name = ''
+        self.area_code = ''
+        self.area_name = ''
+        self.carrier = ''
+        self.is_mobile = False
+
+    def __unicode__(self):
+        return "%s" % self.__dict__
 
     def parse(self):
 
         phone = phonenumbers.parse(self.phone, "ID")
+
         # 1st pass, try to find mobile carrier from phonenumbers
+        self.carrier = phonenumbers.carrier.name_for_number(phone, "en")
 
-        self.mobile_provider = carrier.name_for_number(phone, "en")
-
-        if self.mobile_provider:
+        if self.carrier:
             self.is_mobile = True
+            return
         else:
-            national_number = phone.national_number
+            national_number = str(phone.national_number)
+            number_length = len(national_number)
 
             # 1. Try if area code is supplied with CDMA_PREFIX_SPECIAL_AREA_CODE
-            if CDMA_PREFIX_SPECIAL_AREA_CODE.get(str(national_number[:4]), False):
+            carrier = CDMA_PREFIX_SPECIAL_AREA_CODE.get(national_number[:4])
+            if carrier:
                 self.is_mobile = True
+                self.carrier = carrier
                 return
 
-            # 2. Try CDMA_PREFIX_WITH_AREA_CODE, assume phone with area code
+            # 2. Check if it's length include area code or not
+            # Without area code, must be 8
+            # With area code, can be 10 or 11
+
+            if number_length not in [8, 10, 11]:
+                self.is_mobile = False
+                return
+            else:
+                # assume without area code, check prefix directly
+                if number_length == 8:
+                    carrier = CDMA_PREFIX.get(national_number[:2]) or CDMA_PREFIX.get(national_number[:1])
+                    area_code = ''
+                    area_name = ''
+
+                # Check if area_code is valid first
+                elif number_length == 10:
+                    area_code = national_number[:2]
+                    area_name = AREA_CODE.get(area_code)
+                    if area_name:
+                        carrier = CDMA_PREFIX.get(national_number[2:3]) or CDMA_PREFIX.get(national_number[2:4])
+
+                elif number_length == 11:
+                    area_code = national_number[:3]
+                    area_name = AREA_CODE.get(area_code)
+                    if area_name:
+                        carrier = CDMA_PREFIX.get(national_number[3:4]) or CDMA_PREFIX.get(national_number[3:5])
+
+                if carrier:
+                    self.is_mobile = True
+                    self.carrier = carrier
+                    self.area_code = area_code
+                    self.area_name = area_name
