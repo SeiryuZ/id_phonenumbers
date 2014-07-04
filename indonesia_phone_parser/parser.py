59aa018da878e6a7bedf67014588f4e18de1a8cf
Steven <steven@ui.co.id>
2014-07-04 16:20:42 +0700

    Fix comments

--- indonesia_phone_parser/parser.py
+++ indonesia_phone_parser/parser.py
@@ -25,29 +25,29 @@ class Parser(object):
 
     def get_area_code(self):
         if self.phone:
-            # Check for area_code with 3 area_code prefix first
+            # Test for 3-number area code
             self.area_name = AREA_CODE.get(self.national_number[:3])
             if self.area_name is not None:
                 self.area_code = self.national_number[:3]
                 self.regional_number = self.national_number[3:]
 
-            # Check for area_code with 2 area_code prefix first
+            # Test for 2-number area code
             else:
                 self.area_name = AREA_CODE.get(self.national_number[:2])
                 if self.area_name:
                     self.area_code = self.national_number[:2]
                     self.regional_number = self.national_number[2:]
 
-            # If cannot parse Area Name, regional number is given without area code
+            # If cannot determine Area Name,  assume given number is regional number
             if self.area_name is None:
                 self.regional_number = self.national_number
 
         return None
 
     def parse(self):
-
-        # get _area_code
         self.get_area_code()
+
+        # TODO: Check Mobile Carrier Prefix
 
         # Try to check CDMA prefix
         if len(self.regional_number) == 8:
