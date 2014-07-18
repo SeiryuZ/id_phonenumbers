d50faf8774fe615323343a6be22596f7d76b0491
Steven <steven@ui.co.id>
2014-07-18 11:42:42 +0700

    Fix setup and version numbering

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='id_phonenumbers',
     packages=['id_phonenumbers'],
-    version='0.1',
+    version='0.1.0',
     description='Indonesian Phonenumbers Parser',
     license='MIT',
     author='Steven',
     author_email='stvn.bu@gmail.com',
     url='https://github.com/SeiryuZ/id_phonenumbers',
-    download_url='https://github.com/SeiryuZ/id_phonenumbers/tarball/0.1',
+    download_url='https://github.com/SeiryuZ/id_phonenumbers/tarball/0.1.0',
     keywords=['phonenumbers', 'Indonesia'],
+    install_requires=['phonenumbers'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
