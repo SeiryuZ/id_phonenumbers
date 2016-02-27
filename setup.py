4d05b061c5eec2745c075d40b88b875af826a18f
Steven <SeiryuZ@hotmail.com>
2016-02-27 12:48:42 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='id_phonenumbers',
     packages=['id_phonenumbers'],
-    version='0.1.2',
+    version='0.1.3',
     description='Indonesian Phonenumbers Parser',
     license='MIT',
     author='Steven',
