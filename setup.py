742d7195d04c904f79cba19ea8b35914d938d118
Steven <SeiryuZ@hotmail.com>
2018-01-03 15:42:29 +0700

    Bump to 0.3.0, do not use 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='id_phonenumbers',
     packages=['id_phonenumbers'],
-    version='0.2.1',
+    version='0.3.0',
     description='Indonesian Phonenumbers Parser',
     license='MIT',
     author='Steven',
